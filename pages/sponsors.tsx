453172911e5a119e19dde6cfc6e1bf5969d20847
alexandru_constantin_1 <alexandru.constantin-2@student.manchester.ac.uk>
2021-07-18 10:23:58 +0100

    more es-lint issues

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Layout from '../components/layout';
 
 import styles from './sponsors.module.css';
-import type { Sponsor, Partner } from '../lib/sponsors';
+import type { Partner } from '../lib/sponsors';
 
 import { getAllSponsors, getAllPartners } from '../lib/sponsors';
 
@@ -17,40 +17,40 @@ export async function getStaticProps() {
 }
 
 interface IndexParams {
-  sponsors: Sponsor[],
+
   partners: Partner[]
 }
 export default function Sponsors({ partners }: IndexParams) {
   return (
-      <Layout title="Sponsors">
-          <div className={styles.banner}>
-              <h2>Our Sponsors and Partners</h2>
-              <h4>
-                Interested in sponsoring us? 
-                <a href="#contact">Contact us</a> 
+    <Layout title="Sponsors">
+      <div className={styles.banner}>
+            <h2>Our Sponsors and Partners</h2>
+            <h4>
+                Interested in sponsoring us?
+                <a href="#contact">Contact us</a>
                 to find out more!
-              </h4>
-          </div>
-          <h2>Partners:</h2>
-          <div className={styles.eventTable}>
+            </h4>
+            </div>
+        <h2>Partners:</h2>
+        <div className={styles.eventTable}>
             {partners.map(({ name, description, photoSrc }, i) => (
-                <div key={i} className={styles.partnerCard}>
-                  <img style={{ maxHeight: '200px' }} src={photoSrc} alt={name}/>
+              <div key={i} className={styles.partnerCard}>
+                  <img style={{ maxHeight: '200px' }} src={photoSrc} alt={name} />
                   <span style={{ padding: '3em', alignContent: 'center' }}>
                     {description}
                   </span>
                 </div>
             ))}
           </div>
-          <div id="contact" className="container">
+        <div id="contact" className="container">
             <div className="text-center">
               <h2>Contact Us</h2>
-                <p>Feel free to email us any inquiries that you may have!</p>
-              </div>
-              <ul className="row">
-                <li><span>alexandru@unicsmcr.com</span></li>
-                <li><span>mikolaj@unicsmcr.com</span></li>
-              </ul>
+              <p>Feel free to email us any inquiries that you may have!</p>
+            </div>
+            <ul className="row">
+              <li><span>alexandru@unicsmcr.com</span></li>
+              <li><span>mikolaj@unicsmcr.com</span></li>
+            </ul>
           </div>
       </Layout>
   );
