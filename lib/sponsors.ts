1c0e9a96c9e81101bd732e05ab98ad31faa9b8e0
alexandru_constantin_1 <alexandru.constantin-2@student.manchester.ac.uk>
2021-07-18 10:18:47 +0100

    fixed es-lint

--- lib/sponsors.ts
+++ lib/sponsors.ts
@@ -11,14 +11,14 @@ export interface Sponsor {
 export const defaultMemberPhoto = '/images/default_profile.svg';
 
 export function getAllPartners(): Partner[] {
-    return [
-        {
-            name: 'Unity',
-            description: 'Unity is a cross-platform game engine developed by Unity Technologies, first announced and released in June 2005 at Apple Inc.\'s Worldwide Developers Conference as a Mac OS X-exclusive game engine. Unity is so much more than the world’s best real-time development platform – it’s also a robust ecosystem designed to enable your success. Join our dynamic community of creators so you can tap into what you need to achieve your vision.',
-            photoSrc: 'https://b-waterstudios.com/wp-content/uploads/2021/03/unity-logo-1230x690-2.jpg',
-        }
-    ];
+  return [
+    {
+      name: 'Unity',
+      description: 'Unity is a cross-platform game engine developed by Unity Technologies, first announced and released in June 2005 at Apple Inc.\'s Worldwide Developers Conference as a Mac OS X-exclusive game engine. Unity is so much more than the world’s best real-time development platform – it’s also a robust ecosystem designed to enable your success. Join our dynamic community of creators so you can tap into what you need to achieve your vision.',
+      photoSrc: 'https://b-waterstudios.com/wp-content/uploads/2021/03/unity-logo-1230x690-2.jpg',
+    },
+  ];
 }
 export function getAllSponsors(): Sponsor[] {
-    return [];
+  return [];
 }
--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -3,52 +3,55 @@ import React from 'react';
 import Layout from '../components/layout';
 
 import styles from './sponsors.module.css';
-import type { Sponsor } from '../lib/sponsors';
-import type { Partner } from '../lib/sponsors';
+import type { Sponsor, Partner } from '../lib/sponsors';
+
 import { getAllSponsors, getAllPartners } from '../lib/sponsors';
 
 export async function getStaticProps() {
-    return {
-        props: {
-            sponsors: getAllSponsors(),
-            parteners: getAllPartners(),
-        },
-    };
+  return {
+    props: {
+      sponsors: getAllSponsors(),
+      parteners: getAllPartners(),
+    },
+  };
 }
 
 interface IndexParams {
-    sponsors: Sponsor[],
-    partners: Partner[]
+  sponsors: Sponsor[],
+  partners: Partner[]
 }
-export default function Sponsors({ sponsors, partners }: IndexParams) {
-    return (
-        <Layout title="Sponsors">
-            <div className={styles.banner}>
-                <h2>Our Sponsors and Partners</h2>
-                <h4>Interested in sponsoring us? <a href="#contact">Contact us</a> to find out more!</h4>
-            </div>
-            <h2>Partners:</h2>
-            <div className={styles.eventTable}>
-                {partners.map(({ name, description, photoSrc }, i) => (
-                    <div key={i} className={styles.partnerCard}>
-                        <img style={{ maxHeight: '200px' }} src={photoSrc} alt={name}>
-                        </img>
-                        <span style={{ padding: '3em', alignContent: 'center' }}>
-                            {description}
-                        </span>
-                    </div>
-                ))}
-            </div>
-            <div id="contact" className="container">
-                <div className="text-center">
-                    <h2>Contact Us</h2>
-                    <p>Feel free to email us any inquiries that you may have!</p>
+export default function Sponsors({ partners }: IndexParams) {
+  return (
+      <Layout title="Sponsors">
+          <div className={styles.banner}>
+              <h2>Our Sponsors and Partners</h2>
+              <h4>
+                Interested in sponsoring us? 
+                <a href="#contact">Contact us</a> 
+                to find out more!
+              </h4>
+          </div>
+          <h2>Partners:</h2>
+          <div className={styles.eventTable}>
+            {partners.map(({ name, description, photoSrc }, i) => (
+                <div key={i} className={styles.partnerCard}>
+                  <img style={{ maxHeight: '200px' }} src={photoSrc} alt={name}/>
+                  <span style={{ padding: '3em', alignContent: 'center' }}>
+                    {description}
+                  </span>
                 </div>
-                <ul className="row">
-                    <li><span>alexandru@unicsmcr.com</span></li>
-                    <li><span>mikolaj@unicsmcr.com</span></li>
-                </ul>
-            </div>
-        </Layout>
-    );
+            ))}
+          </div>
+          <div id="contact" className="container">
+            <div className="text-center">
+              <h2>Contact Us</h2>
+                <p>Feel free to email us any inquiries that you may have!</p>
+              </div>
+              <ul className="row">
+                <li><span>alexandru@unicsmcr.com</span></li>
+                <li><span>mikolaj@unicsmcr.com</span></li>
+              </ul>
+          </div>
+      </Layout>
+  );
 }
